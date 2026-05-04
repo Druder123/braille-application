0605b46bde319a06b3e3607dd6eadc1c9fe61462
abdir <abdirugu123@gmail.com>
2026-05-04 14:35:25 -0600

    Version 1

--- src/brailleapplication/BrailleEnum.java
+++ src/brailleapplication/BrailleEnum.java
@@ -11,7 +11,10 @@ package brailleapplication;
 public enum BrailleEnum {
     A("100000"), B("110000"), C("100100"), D("100110"), E("100010"),
     F("110100"), G("110110"), H("110010"), I("010100"), J("010110"),
-    ESPACIO("000000");
+    K("101000"), L("111000"), M("101100"), N("101110"), O("101010"),
+    P("111100"), Q("111110"), R("111010"), S("011100"), T("011110"),
+    U("101001"), V("111001"), W("010111"), X("101101"), Y("101111"),
+    Z("101011"), ESPACIO("000000");
 
     private final String patron;
 
@@ -26,7 +29,7 @@ public enum BrailleEnum {
     public static String getPatronFromName(char letter) {
         String patron = "No existe";
         for(BrailleEnum braille : BrailleEnum.values()) {
-            if(braille.name().equals(letter)) {
+            if(braille.name().charAt(0) == letter) {
                 patron = braille.getPatron();
                 break;
             }
