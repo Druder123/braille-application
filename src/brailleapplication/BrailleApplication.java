6f2cd9a0abd45f6ba092bb0df6b7b829f903bef6
abdir <abdirugu123@gmail.com>
2026-05-04 08:58:07 -0600

    hola mundo

--- src/brailleapplication/BrailleApplication.java
+++ src/brailleapplication/BrailleApplication.java
@@ -14,7 +14,7 @@ public class BrailleApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola dannyyyy!");
+        System.out.println("Hola mundo!");
     }
     
 }
